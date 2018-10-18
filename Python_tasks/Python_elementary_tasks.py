270830abfac4ffeb6806fab59b9b754f2f1adbee
Szymon Słomiński <simon.slominski@gmail.com>
2018-10-18 17:05:25 +0200

    Update Python_elementary_tasks.py

--- Python_tasks/Python_elementary_tasks.py
+++ Python_tasks/Python_elementary_tasks.py
@@ -164,3 +164,19 @@ def spy_game(nums):
             code.pop(0)  # code.remove(num) also works
 
     return len(code) == 1
+
+"""
+TASK 12
+
+Create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+#Example
+# filter_list([1,2,'a','b']) == [1,2]
+# filter_list([1,'a','b',0,15]) == [1,0,15]
+# filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+"""
+
+def filter_list(l):
+    new_list = [e for e in l if isinstance(e, int)]
+    return new_list
+
