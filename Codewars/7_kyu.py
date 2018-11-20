ac48870661fa895afddc64538e559b38400ef361
Szymon Słomiński <simon.slominski@gmail.com>
2018-11-20 15:52:49 +0100

    Update 7_kyu.py

--- Codewars/7_kyu.py
+++ Codewars/7_kyu.py
@@ -11,6 +11,31 @@ but years divisible by 100 are no leap years
 but years divisible by 400 are leap years
 """
 
-
 def isLeapYear(year):
     return (year % 100 != 0 and year % 4 == 0) or year % 400 == 0
+
+
+"""
+TASK: List Filtering
+
+In this kata you will create a function that takes a list of non-negative integers and strings and 
+returns a new list with the strings filtered out.
+
+Example:
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+"""
+
+def filter_list(l):
+    list = []
+    for item in l:
+        if type(item) == int:
+            list.append(item)
+    return list
+
+
+# def filter_list(l):
+#     # More advanced (Pythonic) solution:
+#     return [x for x in l if type(x) is not str]
+
