30d891dbdbb25728d40687f430e52c3e23a0f58f
Szymon Słomiński <simon.slominski@gmail.com>
2018-11-22 14:05:18 +0100

    Update 8_kyu.py

--- Codewars/8_kyu.py
+++ Codewars/8_kyu.py
@@ -18,3 +18,21 @@ def solution(a, b):
         return b+a+b
 
     
+"""
+TASK: Is this my tail?
+
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails.
+To help her, you must correct the broken function to make sure that the second argument (tail), 
+is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+The arguments will always be strings, and normal letters.
+
+Example:
+assert_equals(correct_tail("Fox", "x"), True)
+assert_equals(correct_tail("Giraffe", "d"), False)
+"""
+
+def correct_tail(body, tail):
+    return body[-1] == tail[0]
+
